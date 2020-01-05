06667c0074496f80533a20beec09b6f6a29b5154
Javier <moubayyed47@hotmail.com>
2020-01-05 15:14:25 +0100

    unnecessary comments & spaces removed

--- IMDB/models.py
+++ IMDB/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
-# Create your models here.
-
 class Pelicula(models.Model):
+	"""Modelo que definie a una pelicula"""
+	
 	titulo=models.CharField(max_length=50)
 	descripcion = models.TextField()
 	created_at = models.DateTimeField(auto_now_add=True)
--- IMDB/serializers.py
+++ IMDB/serializers.py
@@ -4,7 +4,6 @@ from .models import Pelicula
 from django.contrib.auth.models import User
 from . import models
 
-
 class PeliculaSerializer(serializers.ModelSerializer):
 
 	class Meta:
